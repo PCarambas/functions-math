179c40b3916f3ff7245d81c2ab9d1a4b5ee3c224
Peggy Carambas <PCarambas@gmail.com>
2022-10-07 10:35:55 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// Write a function to add two values
+// Add the values of the two parameters and return the results
+
+const addValues = (num1, num2) => {
+    return num1 + num2
+}
+
+
+
+
+
+// Write a function to multiply a single number by 2
+// Multiply the value in the parameter by 2 and return the result.
+
+const multiply = (number) => {
+    return  number * 2
+}
+
+const sum = addValues(4, 14)
+const total = multiply(sum)
+
+console.log(total)
